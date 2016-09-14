6e6b7318d35eb79a3053b17344cec976cec378b6
janhuang <bboyjanhuang@gmail.com>
2016-09-14 12:02:37 +0800

    add annotation execute function

--- tests/AnnotationTest.php
+++ tests/AnnotationTest.php
@@ -1,19 +1,6 @@
 <?php
 use FastD\Annotation\Annotation;
 
-/**
- * Created by PhpStorm.
- * User: janhuang
- * Date: 16/4/26
- * Time: 下午3:14
- * Github: https://www.github.com/janhuang
- * Coding: https://www.coding.net/janhuang
- * SegmentFault: http://segmentfault.com/u/janhuang
- * Blog: http://segmentfault.com/blog/janhuang
- * Gmail: bboyjanhuang@gmail.com
- * WebSite: http://www.janhuang.me
- */
-
 class AnnotationTest extends PHPUnit_Framework_TestCase
 {
     public function setUp()
